52c247c223e385a482510d004c69f6e852cc86a2
Colin Aulds <caulds989@gmail.com>
2017-11-20 12:06:02 -0800

    add show/hide features to menu and clickable home button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
   $(".dropdown-button").dropdown({hover: true});
+
+  //goes to home page
+  $(".logo").click(function() {
+    $(".defaultPage").show();
+    $(".contact").hide();
+    $(".profile").hide();
+  });
+
+  //show profile page, hide contact
+  $(".item1").click(function() {
+    $(".defaultPage").hide();
+    $(".contact").hide();
+    $(".profile").show();
+  });
+
+  //show contact page, hide profile
+  $(".item2").click(function() {
+    $(".defaultPage").hide();
+    $(".contact").show();
+    $(".profile").hide();
+  });
 })
